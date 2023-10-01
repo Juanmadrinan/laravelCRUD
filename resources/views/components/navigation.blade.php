434dca6580ae6fbc8af86bb22ed3710881a1eb97
Juanmadrinan <jmadrinanpinzon@gmail.com>
2023-10-01 07:16:51 -0500

    Ajustes finales

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -4,8 +4,7 @@
             <div class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
                 <div class="flex items-center flex-shrink-0">
                     <a href="{{ route('home') }}">
-                        <svg class="w-8 h-8 text-sky-500" fill="none" width="0" stroke="currentColor"
-                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <h4 class="px-3 py-2 text-start font-medium rounded-md hover:text-sky-600 dark:hover:text-white text-sky-600 dark:text-white : 'text-slate-400' ">Project Laravel</h4>
                             <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
                             <path
                                 d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z">
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,9 @@
 <x-app :title="$post->title" :meta-description="$post->body">
     <h1 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500">{{ $post->title }}</h1>
     <div class="flex flex-col max-w-xl px-8 py-4 mx-auto bg-white rounded shadow h-96 dark:bg-slate-800">
-        <p class="flex-1 leading-normal text-slate-600 dark:text-slate-400">{{ $post->body }}</p>
+        <h4 class="my-4 font-serif text-3xl text-center text-sky-600 dark:text-sky-500">{{$post->body}}</h4>
+
+        <p class="flex-1 leading-normal text-slate-600 dark:text-slate-400">Datos en JSON: {{ ($post) }}</p>
 
         <a class="mr-auto text-sm font-semibold underline border-2 border-transparent rounded dark:text-slate-300 text-slate-600 focus:border-slate-500 focus:outline-none" href="{{ route('posts.index') }}">Regresar</a>
     </div>
